9b6d4506664ee136a14161bf9492f885936f5576
joel <joel@joel-OptiPlex-790>
2021-10-10 23:19:35 -0500

    cleaned shit up

--- ARMA.py
+++ ARMA.py
@@ -13,52 +13,14 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-ar1 = np.array([1, 0.33])
-ma1 = np.array([1, 0.9])
-simulated_ARMA_data = ArmaProcess(ar1, ma1).generate_sample(nsample=10000)
-wSPY = pd.read_csv('weekly_SPY.csv', sep=',')
+wSPY = pd.read_csv('daily_SPY_test.csv', sep=',')
 
-
-
-
-
-
-'''
-plt.figure(figsize=[15, 7.5]); # Set dimensions for figure
-plt.plot(simulated_ARMA_data)
-plt.title("Simulated ARMA(1,1) Process")
-plt.xlim([0, 200])
-plt.show()
-'''
 closePrice = list(reversed(wSPY['close']))
-
-#print(simulated_ARMA_data)
-#print(closePrice)
-plt.figure(figsize=[15, 7.5]); # Set dimensions for figure
-plt.plot(closePrice)
-plt.title("Weekly SPY")
-plt.xlim([0, 1143])
-plt.show()
 
 diffPrice = list()
 for i in range(1,len(closePrice)-2):
     diffPrice.append(closePrice[i]-closePrice[i-1])
 
-print(diffPrice)
-
-plt.figure(figsize=[15, 7.5]); # Set dimensions for figure
-plt.plot(diffPrice)
-plt.title("differenced SPY")
-plt.xlim([0, 1143])
-plt.show()
-
-
-#plot_pacf(simulated_ARMA_data);
-#plt.show()
-
-#plot_acf(simulated_ARMA_data);
-
-#plot_pacf(closePrice)
 # Augmented Dickey-Fuller test
 
 #data = pd.read_csv('jj.csv')
@@ -67,7 +29,31 @@ plt.show()
 #print(f'p-value: {ad_fuller_result[1]}')
 
 acf_1 =  acf(diffPrice, nlags=7)
-print(acf_1)
+pacf_1 = pacf(diffPrice, nlags=7)
+print(acf_1[1:])
+print(max(abs(acf_1[1:])))
+
+fig = plt.figure()
+plt.subplot(2,2,1)
+#plt.figure(figsize=[15, 7.5]); # Set dimensions for figure
+plt.plot(closePrice)
+plt.title("daily SPY")
+#plt.xlim([0, 1143])
+
+plt.subplot(2,2,2)
+#plt.figure(figsize=[15, 7.5]); # Set dimensions for figure
+plt.plot(diffPrice)
+plt.title("differenced SPY")
+#plt.xlim([0, 1143])
+
+plt.subplot(2,2,3)
 plt.plot(acf_1)
+plt.title("ACF plot")
+plt.ylim([-0.5,0.5])
+
+plt.subplot(2,2,4)
+plt.plot(pacf_1)
+plt.title("PACF plot")
+plt.ylim([-0.5,0.5])
 plt.show()
 
